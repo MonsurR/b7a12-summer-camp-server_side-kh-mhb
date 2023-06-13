66a3dfa5bd983c2740798e365e3c857bb65e1a07
Mahmudul Hasan <mahmudulhasansec65@gmail.com>
2023-06-13 17:33:42 +0600

    signup user creation

--- index.js
+++ index.js
@@ -1,9 +1,91 @@
 const express = require('express')
-const cors = require('cors');
 const app = express()
-const port = process.env.PORT || 5000;
-app.use(cors());
-app.use(express.json());
+const port = process.env.PORT || 5000
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
+const cors = require('cors')
+const jwt = require('jsonwebtoken');
+const { query } = require('express');
+app.use(cors())
+app.use(express.json())
+
+
+const username = process.env.MONGO_USR;
+const password = process.env.MONGO_PASS;
+
+const uri = `mongodb+srv://mahmudulhasanw3b:${password}@cluster0.37udjhi.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+function verifyJWT(req, res, next) {
+    // console.log(`token inside verifyjwt `, req.headers.authorization);    
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        res.status(401).send('unauthorized access')
+    }
+    console.log(authHeader)
+    const token = authHeader.split(' ')[1];
+    if (token === null) {
+        console.log('in token')
+        res.status(401).send('unauthorized access')
+
+    }
+    console.log(token)
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded;
+        console.log(decoded)
+        next()
+    });
+
+}
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+
+        const userCollection = client.db('sterio').collection('user');
+
+
+
+        app.put('/user', async (req, res) => {
+            await client.connect();
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    email: user.email,
+                    role: user.role,
+                    name: user.name,
+                    img: user.img,
+                    verifiedSeller: false
+                },
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            // console.log(result);
+            res.send(result)
+
+
+        })
+
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send('Simple Curd!')
